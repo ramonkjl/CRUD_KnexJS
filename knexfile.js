d62f32300e597b3e415103b3eb8957d845bf2551
Ramon Nunes <ramonkjl@gmail.com>
2021-01-20 22:23:18 -0300

    configurando Knex.js/Knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      database: "knex",
+      user: "ramon",
+      password: "123456"
+    }
+  },
+};
